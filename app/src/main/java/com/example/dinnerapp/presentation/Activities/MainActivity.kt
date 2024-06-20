5075277356e380d0d668bdfda57569237e6322d4
omar boshra <osarious2@gmail.com>
2024-06-20 17:33:53 +0800

    BFMT-1: Add MainActivity and its parent BaseActivity

---
+++ app/src/main/java/com/example/dinnerapp/presentation/Activities/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.dinnerapp.presentation.Activities
+
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import com.example.dinnerapp.R
+import com.example.dinnerapp.databinding.ActivityMainBinding
+import com.example.dinnerapp.presentation.epoxy.events.activityevents.MainActivityEvents
+import com.example.dinnerapp.presentation.utils.NavigationEvents
+import com.example.dinnerapp.presentation.viewmodels.MainActivityViewModel
+import com.example.dinnerapp.utils.launchAndRepeatWithViewLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
+        get() = ActivityMainBinding::inflate
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    override fun setup() {
+        launchAndRepeatWithViewLifecycle {
+            launch { observeNavigationEvents() }
+            launch { onActivityCreate() }
+        }
+    }
+
+    // region activity events .
+    private fun onActivityCreate() {
+        viewModel.onEvent(MainActivityEvents.OnActivityCreate(NavigationEvents.ToMealCategories))
+    }
+
+    //endregion
+
+    // region activity observers .
+    private suspend fun observeNavigationEvents() {
+        viewModel.navigationEvents.collect {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.main_content) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController
+                .setGraph(R.navigation.nav_graph)
+        }
+    }
+
+    //endregion
+}
