6dd6cabc362ac82663f8060072fab3f9c86f82bf
omar boshra <osarious2@gmail.com>
2024-06-20 17:35:29 +0800

    BFMT-1: Add MealCategoriesFragment and its parent BaseFragment

---
+++ app/src/main/java/com/example/dinnerapp/presentation/fragments/MealCategoriesFragment.kt
@@ -0,0 +1,76 @@
+package com.example.dinnerapp.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.dinnerapp.databinding.FragmentMealCategoriesBinding
+import com.example.dinnerapp.presentation.epoxy.controllers.MealCategoriesEpoxyController
+import com.example.dinnerapp.presentation.epoxy.events.fragmentevents.MealCategoriesFragmentEvents
+import com.example.dinnerapp.presentation.utils.ListState
+import com.example.dinnerapp.presentation.viewmodels.MealCategoriesViewModel
+import com.example.dinnerapp.utils.Constants
+import com.example.dinnerapp.utils.launchAndRepeatWithViewLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MealCategoriesFragment : BaseFragment<FragmentMealCategoriesBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean)
+    -> FragmentMealCategoriesBinding
+        get() = FragmentMealCategoriesBinding::inflate
+
+    private val viewModel: MealCategoriesViewModel by viewModels()
+    private lateinit var controller: MealCategoriesEpoxyController
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        // Setting up the controller here in order to listen to events through the viewmodel.
+        controller = MealCategoriesEpoxyController(listener = viewModel)
+
+        launchAndRepeatWithViewLifecycle {
+            launch { observeMealCategories() }
+            launch { observeMealCategoriesOnFailure() }
+            launch { observeNavigationEvents() }
+            launch { initializeUI() }
+            launch { onFragmentStart() }
+        }
+    }
+
+    // region observables .
+    private suspend fun observeMealCategories() {
+        viewModel.mealListSuccessFlow.collect {
+            controller.setLoadedState(it)
+        }
+    }
+
+    private suspend fun observeMealCategoriesOnFailure() {
+        viewModel.mealListErrorFlow.collect {
+            controller.setErrorState(ListState.Error(Constants.StateConstants.ERROR))
+        }
+    }
+
+    private suspend fun observeNavigationEvents() {
+        viewModel.navigationEvents.collect {
+        }
+    }
+
+    //endregion
+
+    // region Fragment Events
+
+    private fun initializeUI() {
+        binding.epoxyRv.setController(controller)
+        controller.setLoadingState(ListState.Loading(Constants.StateConstants.LOADING))
+    }
+
+    private fun onFragmentStart() {
+        viewModel.onEvent(MealCategoriesFragmentEvents.OnFragmentCreate)
+    }
+
+    //endregion
+}
