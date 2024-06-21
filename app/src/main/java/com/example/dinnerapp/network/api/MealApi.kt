eb55a97d120ad4071a88d75dd3d17f7cbde1f4b2
omar boshra <osarious2@gmail.com>
2024-06-21 09:15:40 +0800

    BFMT-1: Add MealApi

---
+++ app/src/main/java/com/example/dinnerapp/network/api/MealApi.kt
@@ -0,0 +1,11 @@
+package com.example.dinnerapp.network.api
+
+import com.example.dinnerapp.network.models.MealCategoriesResponse
+import com.example.dinnerapp.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MealApi {
+    @GET(Constants.MealConstants.MEAL_CATEGORIES_URL)
+    suspend fun getMealCategoriesList(): Response<MealCategoriesResponse>
+}
