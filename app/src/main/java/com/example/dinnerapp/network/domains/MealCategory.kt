043c40cca0fd05fb36ad2c7f4d77033eef7a4bee
omar boshra <osarious2@gmail.com>
2024-06-21 09:18:12 +0800

    BFMT-1: Add MealCategory domain data class

---
+++ app/src/main/java/com/example/dinnerapp/network/domains/MealCategory.kt
@@ -0,0 +1,29 @@
+package com.example.dinnerapp.network.domains
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MealCategory(
+    val idMealCategory: String? = "",
+    val mealCategoryName: String? = "",
+    val mealCategoryUrl: String? = "",
+    val mealCategoryDescription: String? = "",
+) : Parcelable {
+    /**
+     * Get category description
+     *
+     * @return concise description with first line and up-to a punctuation
+     */
+    fun getCategoryConciseDescription(): String? {
+        val lines = mealCategoryDescription?.lines()
+        val firstLine = lines?.take(1)
+        val rawText = firstLine?.joinToString("\n")
+        val endIndex = rawText?.indexOfFirst { it == ',' || it == '.' || it == ';' }
+        return if (endIndex != -1) {
+            endIndex?.let { rawText.substring(0, it) }
+        } else {
+            rawText
+        }
+    }
+}
